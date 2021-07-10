e43ba5377a92b763bd195c2382513db085917e3a
Toktam <ttmohebbi@gmail.com>
2021-07-10 03:42:01 +0000

    memo or useMemo not working

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,11 +1,24 @@
+import { useMemo } from 'react';
 import CountryPicker from './CountryPicker';
 import CountryDetails from './CountryDetails';
 
-export default function HomeContent() {
+const HomeContent = useMemo(() => {
   return (
     <div className='container'>
       <CountryDetails />
       <CountryPicker />
     </div>
-  )
-}
+  );
+}, []);
+
+// export default HomeContent;
+
+// export default HomeContent = memo(() => {
+//   return (
+//     <div className='container'>
+//       <CountryDetails />
+//       <CountryPicker />
+//     </div>
+//   );
+// });
+
