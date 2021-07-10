d8532fe04c6ee4daffffb201c995d141b259b0c1
Toktam <ttmohebbi@gmail.com>
2021-07-10 02:11:17 +0000

    first step of creating a context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      Home Sweet Home
       <Home />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,20 @@ import { useState } from 'react';
 import CountryPicker from './CountryPicker';
 import CountryDetails from './CountryDetails';
 
-
 import './Home.css'
 
+//step 3: to convert the lifted state into global state we create a context
+const CountryContext = createContext();
 
 export default function Home() {
 
+  //step 2: lifted state from the child component into the parent component
   const [country, setCountry] = useState('CA');
 
   return (
-    <div className='container'>
-      Home Sweet Home
-      <div>
+    <div className='container'>      
       <CountryDetails country={country} />
       <CountryPicker country={country} setCountry={setCountry} />
-      </div>
     </div>
   )
 }
