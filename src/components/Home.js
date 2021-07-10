8e2d4925a0a0656dfed70b2da13790bbd73e3aff
Toktam <ttmohebbi@gmail.com>
2021-07-10 02:15:52 +0000

    add comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ import CountryDetails from './CountryDetails';
 import './Home.css'
 
 //step 3: to convert the lifted state into global state we create a context
+//context stores the state at the top of the application and any child below it can
+//reach out and grab it from the parent without it having to be passed down through prop drilling
+//from one component to the next 
 const CountryContext = createContext();
 
 export default function Home() {
