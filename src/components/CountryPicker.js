0f909fefbbfca575f0641d0c08db4a86ba3fe550
Toktam <ttmohebbi@gmail.com>
2021-07-10 02:02:00 +0000

    edit typo

--- src/components/CountryPicker.js
+++ src/components/CountryPicker.js
@@ -7,7 +7,7 @@ export default function CountryPicker({ country, setCountry }) {
   return(
     <select value={country} onChange={(event) => setCountry(event.target.value) }>
       <option value="CA">Canada</option>
-      <option calue="CO">Colombia</option>
+      <option value="CO">Colombia</option>
     </select>
   )
 }
